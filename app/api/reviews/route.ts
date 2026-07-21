dbcff6abbc190b3cf074e7a80bb9000ce680e0e3
mebecassidy-beep <mebecassidy@gmail.com>
2026-07-20 21:33:21 -0700

    Update route.ts

--- app/api/reviews/route.ts
+++ app/api/reviews/route.ts
@@ -1,3 +1,4 @@
+export const dynamic = 'force-dynamic';
 import { createRouteHandlerClient } from '@supabase/auth-helpers-nextjs'
 import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
